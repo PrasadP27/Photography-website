91fbeb9b7ef0471063662e2948d076ad53cc28b3
PrasadP27 <bobpanchal948@gmail.com>
2024-04-02 16:23:13 +0530

    Cursor loading

--- script.js
+++ script.js
@@ -2,10 +2,12 @@
 const loader = document.querySelector('.loader')
 
 window.addEventListener('load', () => {
-    loader.style.opacity = '0'
+    loader.style.opacity = '0',
+    document.body.style.cursor = 'wait'
     
     setTimeout(() => {
-        loader.style.display = 'none'
+        loader.style.display = 'none',
+        document.body.style.cursor = 'auto'
     }, 200)
 })
 
